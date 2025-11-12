2aa4033bce78016623e2ec7a3828057da6e933c7
dreaesposito <dreaesposito@hotmail.com>
2025-11-11 21:16:25 -0600

    chore(ui): add shadcn theme toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import ThemeToggle from "$lib/ThemeToggle.svelte";
+</script>
+
+<ThemeToggle />
 <h1 class="text-green-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>

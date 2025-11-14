788e75d8799d27c1be7a3d5aba6c73b70d957722
dreaesposito <dreaesposito@hotmail.com>
2025-11-14 11:05:11 -0600

    chore(ui): add styles prop to ThemeToggle

--- src/lib/ThemeToggle.svelte
+++ src/lib/ThemeToggle.svelte
@@ -5,24 +5,30 @@
   import { resetMode, setMode } from "mode-watcher";
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
   import { buttonVariants } from "$lib/components/ui/button/index.js";
+
+  let {styles} = $props();
 </script>
 
-<DropdownMenu.Root>
-  <DropdownMenu.Trigger
-    class={buttonVariants({ variant: "outline", size: "icon" })}
-  >
-    <SunIcon
-      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
-    />
-    <MoonIcon
-      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
-    />
-    <span class="sr-only">Toggle theme</span>
-  </DropdownMenu.Trigger>
-  <DropdownMenu.Content align="end">
-    <DropdownMenu.Item onclick={() => setMode("light")}>Light</DropdownMenu.Item
+<div class={styles}>
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger
+      class={buttonVariants({ variant: "outline", size: "icon" })}
     >
-    <DropdownMenu.Item onclick={() => setMode("dark")}>Dark</DropdownMenu.Item>
-    <DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
-  </DropdownMenu.Content>
-</DropdownMenu.Root>
+      <SunIcon
+        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
+      />
+      <MoonIcon
+        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
+      />
+      <span class="sr-only">Toggle theme</span>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content align="end">
+      <DropdownMenu.Item onclick={() => setMode("light")}
+        >Light</DropdownMenu.Item
+      >
+      <DropdownMenu.Item onclick={() => setMode("dark")}>Dark</DropdownMenu.Item
+      >
+      <DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
+</div>

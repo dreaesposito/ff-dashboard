f9c4a6ce773e8ed48c23272fcd077102cdbd2360
dreaesposito <dreaesposito@hotmail.com>
2025-11-25 08:39:59 -0600

    chore(ui): Refine PlayerColumnDisplay
    - Use the formatName util function
    - Adjust range for mapping player value to a colour

--- src/lib/components/PlayerColumnDisplay.svelte
+++ src/lib/components/PlayerColumnDisplay.svelte
@@ -1,17 +1,16 @@
 <script>
   import * as ButtonGroup from "$lib/components/ui/button-group/index.js";
   import { Button } from "$lib/components/ui/button";
+  import { formatName } from "$lib/utils";
   let { playerObj } = $props();
 
   // map input value to a colour
   function color(n) {
-    const normalized = (n + 1500) / (2000 + 1500); // (-1500, 2000) is the range
+    const normalized = (n + 1500) / (2200 + 1800); // (-1500, 2200) is the range
     const r = Math.floor(255 * (1 - normalized)); // Red colour slider
     const g = Math.floor(255 * normalized); // Green colour slider
     return `rgb(${r}, ${g}, 0)`;
   }
-
-  let formatName = (full) => `${full[0]}. ${full.split(" ")[1]}`;
 </script>
 
 <div class="my-1">

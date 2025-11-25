5144ee928ff921852c1e05b30ebe6a8f68a48082
dreaesposito <dreaesposito@hotmail.com>
2025-11-25 08:32:25 -0600

    feat(ui): Refine TeamCard styling

--- src/lib/components/TeamCard.svelte
+++ src/lib/components/TeamCard.svelte
@@ -5,7 +5,15 @@
   import * as Accordion from "$lib/components/ui/accordion/index.js";
   import TeamTableDisplay from "./TeamTableDisplay.svelte";
 
-  let { teamName, players, onTeamClick, totalValue, trendValue, rank } = $props();
+  let {
+    class: className = "",
+    teamName,
+    players,
+    onTeamClick,
+    totalValue,
+    trendValue,
+    rank,
+  } = $props();
 
   let roster = $state([]);
 
@@ -16,25 +24,21 @@
   });
 </script>
 
-<div
-  class="p-2 bg-primary/15 rounded-md hover:bg-primary/35 hover:shadow-xl hover:cursor-pointer selection:bg-primary/35 transition-all duration-200"
+<Accordion.Root
+  type="single"
   onclick={() => onTeamClick(players)}
+  class={`w-full border p-2 rounded-md hover:bg-foreground/5 hover:shadow-lg hover:cursor-pointer transition-all duration-200 ${className}`}
 >
-  <Accordion.Root type="single" class="w-full">
-    <Accordion.Item>
-      <Accordion.Trigger>
-        <div class="text-xl font-bold">
-          {rank}. {teamName}
-          <Button variant="outline" size="sm">{totalValue}</Button>
-          <Button variant="outline" size="sm">{trendValue}</Button>
-        </div>
-      </Accordion.Trigger>
-      <Accordion.Content
-        class="flex flex-col gap-4 text-balance"
-        hiddenUntilFound
-      >
+  <Accordion.Item>
+    <Accordion.Trigger class="cursor-pointer ">
+      <div class="text-xl">
+        {rank}. {teamName}
+        <Button variant="outline" size="sm">{totalValue}</Button>
+        <Button variant="outline" size="sm">{trendValue}</Button>
+      </div>
+    </Accordion.Trigger>
+    <Accordion.Content class="flex flex-col gap-4 text-balance">
       <TeamTableDisplay {players} />
-      </Accordion.Content>
-    </Accordion.Item>
-  </Accordion.Root>
-</div>
+    </Accordion.Content>
+  </Accordion.Item>
+</Accordion.Root>
